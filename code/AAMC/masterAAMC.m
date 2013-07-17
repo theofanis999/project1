34b4debec25ba4503af0220a76668836eea0c04a
localhost <localhost@localhost.localdomain>
2013-07-18 00:54:44 +0300

    Master script to run script AAMC with different arguments

---
+++ code/AAMC/masterAAMC.m
@@ -0,0 +1,14 @@
+clear
+clc
+
+AAMC(1)
+filename = ['AAMC_' num2str(aggressiveness) 'metric7.mat']
+save(filename);
+
+AAMC(2)
+filename = ['AAMC_' num2str(aggressiveness) 'metric7.mat']
+save(filename);
+
+AAMC(3)
+filename = ['AAMC_' num2str(aggressiveness) 'metric7.mat']
+save(filename);
